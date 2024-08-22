d0b42eb76d262754d359d7367f55a2dc911c9b2b
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2024-08-22 20:16:35 +0530

    fixed the useparam suspense error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          {children}
+          <Suspense fallback={null}>
+            {children}
+          </Suspense>
 
         </Providers>
       </body>
--- app/message/page.tsx
+++ app/message/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useRef } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import Link from 'next/link';
+
 import { useSession } from 'next-auth/react';
 import Loading from '../laoding';
 
